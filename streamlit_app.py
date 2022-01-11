7299d2f2684ae96bf67e64231c26adcee925a89e
t-kalisch <97513797+t-kalisch@users.noreply.github.com>
2022-01-11 18:04:53 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,7 @@ with st.echo(code_location='below'):
     
     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
     num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-    test=st.slider("Test",08.03.2021, datetime.date.today())
+    test=st.slider("Test", "08.03.2021", datetime.date.today(), datetime.date.today())
 
     Point = namedtuple('Point', 'x y')
     data = []
