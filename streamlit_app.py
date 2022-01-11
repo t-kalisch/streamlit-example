28ce2cfac85e96f33007bf11c555ff1b57945e3e
t-kalisch <97513797+t-kalisch@users.noreply.github.com>
2022-01-11 11:36:14 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ import streamlit as st
 
 """
 # Welcome to Streamlit!
-aöslfjsadölfkjasdölfksjadöflsakjfösldkjfösalkfjsadölfjasölfjs
+
 Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
 If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
 forums](https://discuss.streamlit.io).
@@ -16,6 +16,7 @@ In the meantime, below is an example of what you can do with just a few lines of
 
 
 with st.echo(code_location='below'):
+    start_break = st.button("Start break")
     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
     num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
 
