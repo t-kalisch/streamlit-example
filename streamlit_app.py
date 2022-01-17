b6bc5f0d13ad2d7105e0f9d316c078598c8b1a22
t-kalisch <97513797+t-kalisch@users.noreply.github.com>
2022-01-17 19:50:49 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -70,29 +70,26 @@ with st.echo(code_location='below'):
 
    
     
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+    #total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
+    #num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
     #test=st.slider("Test", "08.03.2021", "11.01.2022")
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+    #Point = namedtuple('Point', 'x y')
+    #data = []
 
-    points_per_turn = total_points / num_turns
+    #points_per_turn = total_points / num_turns
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+    #or curr_point_num in range(total_points):
+    #   curr_turn, i = divmod(curr_point_num, points_per_turn)
+    #   angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
+    #   radius = curr_point_num / total_points
+    #   x = radius * math.cos(angle)
+    #   y = radius * math.sin(angle)
+    #   data.append(Point(x, y))
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
-
-coffees_p_M = st.sidebar.button("Coffees per month", help="Show the coffees per month per person", on_click=coffees_per_month())
-add_selectbox = st.sidebar.selectbox("How would you like to be contacted?",("Email", "Home phone", "Mobile phone"))   
+    #t.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
+    #   .mark_circle(color='#0068c9', opacity=0.5)
+    #   .encode(x='x:Q', y='y:Q'))
 
 
 
