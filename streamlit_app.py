24848eadc40b2711ef078209976f9955b8bc5cda
t-kalisch <97513797+t-kalisch@users.noreply.github.com>
2022-01-17 20:55:30 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,17 +44,14 @@ cumulated_coffees=[[19,28,44,63,92,121,153,183,197,238,277,312,349,372,372],[15,
 
 for i in range(15):
     temp=[]
-    temp1=[]
     for j in range(len(monthly_coffees)):
         temp.append(monthly_coffees[j][i])
-        temp1.append(cumulated_coffees[j][i])
     monthly_coffees1.append(temp)
-    cumulated_coffees1.append(temp)
 
 for i in range(15):
     temp=[]
     for j in range(len(cumulatid_coffees)):
-        temp1.append(cumulated_coffees[j][i])
+        temp.append(cumulated_coffees[j][i])
     cumulated_coffees1.append(temp)
     
 
