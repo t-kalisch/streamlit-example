c093d1da538596e08a0958390d0bc759ba62524f
t-kalisch <97513797+t-kalisch@users.noreply.github.com>
2022-01-17 20:49:37 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,14 +38,21 @@ monthly_coffees1=[]
 monthly_coffees=[[19, 9, 16, 19, 29, 31, 32, 30, 14, 41, 39, 34, 37, 24, 10], [15, 6, 6, 20, 29, 20, 24, 25, 29, 22, 32, 30, 35, 18, 12], [13, 6, 12, 16, 25, 35, 28, 37, 31, 27, 36, 30, 22, 14, 0], [10, 3, 7, 12, 27, 36, 37, 15, 22, 44, 10, 6, 4, 7, 1], [18, 1, 18, 21, 34, 35, 35, 26, 21, 43, 43, 27, 36, 22, 9], [0, 0, 0, 0, 19, 27, 23, 9, 5, 16, 22, 17, 26, 17, 0], [0, 0, 0, 0, 0, 12, 18, 8, 5, 13, 2, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0]]
 total_coffees=[372, 314, 328, 242, 382, 183, 58, 1, 3]
 names=['TK','PB','NV','DB','FLG','SHK','TB','TT','RS']
-
 months=["Nov '20", "Dec '20", "Jan '21", "Feb '21", "Mar '21", "Apr '21", "May '21", "Jun '21", "Jul '21", "Aug '21", "Sep '21", "Oct '21", "Nov '21", "Dec '21", "Jan '22"]
+cumulated_coffees1=[]
+cumulated_coffees=[[19,28,44,63,92,121,153,183,197,238,277,312,349,372,372],[15,21,27,47,75,95,119,144,173,195,227,261,296,314,314],[13,19,31,47,72,107,135,172,203,230,266,294,315,328,328],[10,13,20,32,59,96,133,148,170,214,224,230,235,242,242],[18,19,37,58,92,127,162,188,209,252,194,324,360,382,382],[0,0,0,0,19,47,70,79,85,101,123,140,166,183],[0,0,0,0,0,12,30,38,43,56,58,58,58,58,58],[0,0,0,0,0,0,0,0,0,0,0,0,0,1,1],[0,0,0,0,0,0,0,0,0,0,0,0,3,3,3]
+
 for i in range(15):
     temp=[]
     for j in range(len(monthly_coffees)):
         temp.append(monthly_coffees[j][i])
     monthly_coffees1.append(temp)
 
+for i in range(15):
+    temp=[]
+    for j in range(len(total_coffees)):
+        temp.append(total_coffees[j][i])
+    total_coffees1.append(temp)
 
 #col1, buff1, col2, buff2, col3 = st.columns([2,1,2,1,1])
 #user = col1.text_input(label="", placeholder="User")
@@ -56,7 +63,7 @@ for i in range(15):
 
     
 coffees_monthly = st.sidebar.checkbox("Coffees per month")
-if coffees_monthly :
+if coffees_monthly:
     st.header("Coffees per month per person")
     df = pd.DataFrame(monthly_coffees1, columns=names)
     st.line_chart(data=df, width=0, height=0, use_container_width=True)
@@ -69,6 +76,11 @@ if coffees_total:
     st.header("Total coffees")
     st.pyplot(fig1)
 
+coffees_cumulated = st.sidebar.checkbox("Cumulated coffees")
+if coffees_cumulated:
+    st.header("Cumulated coffees")
+    df = pd.DataFrame(cumulated_coffees1, columns=names)
+    st.line_chart(data=df, width=0, height=0, use_container_width=True)
 
     
 #total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
